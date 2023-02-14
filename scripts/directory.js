9d02421d69831b9b88342a15f6987e9de0e45a44
adampotts1 <121840447+adampotts1@users.noreply.github.com>
2023-02-13 23:16:05 -0700

    Add files via upload

---
+++ scripts/directory.js
@@ -0,0 +1,37 @@
+const directory = document.querySelector(".grid");
+
+async function getCompanyData() {
+    const response = await fetch("data/members.json");
+    const data = await response.json();
+    displayProfiles(data);
+};
+
+getCompanyData();
+
+const displayProfiles = (profiles) =>{
+    profiles.forEach((profile) => {
+        let card = document.createElement('section');
+        let image = document.createElement("img");
+        let mobile = document.createElement("p");
+        let name = document.createElement("h3");
+        let website = document.createElement("a");
+        let fulladdress = document.createElement("p");
+
+        
+        name.textContent = profile["companyname"];
+        website.textContent = profile["website"];
+        website.href = profile["website"];
+        mobile.textContent = profile.mobile;
+        fulladdress.textContent = `${profile.address[0]["street"]}, ${profile.address[0]["city"]} ${profile.address[0]["code"]} `;
+        image.src = profile["imagefile"];
+        
+
+        card.appendChild(name)
+        card.appendChild(image);
+        card.appendChild(fulladdress);
+        card.appendChild(mobile);
+        card.appendChild(website);
+
+        directory.appendChild(card);
+    });
+};
